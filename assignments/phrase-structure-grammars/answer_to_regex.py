74ce2dc8dc00161c163ebf5c16f3e7e672a4c854
Logan Walls <2934282+LoganWalls@users.noreply.github.com>
2022-01-26 16:49:48 -0500

    handle non alphabetic characters

--- assignments/phrase-structure-grammars/answer_to_regex.py
+++ assignments/phrase-structure-grammars/answer_to_regex.py
@@ -2,14 +2,14 @@ import re
 
 
 def case_insensitive(char: str) -> str:
-    if char:
+    if char.isalpha():
         return f"[{char.lower()}{char.upper()}]"
     return ""
 
 
 def convert(answer: str) -> str:
     # Escape brackets
-    r = re.sub(r"(\[|\])", lambda m: re.escape(m.group()), answer)
+    r = re.sub(r"([^\sa-zA-Z])", lambda m: re.escape(m.group()), answer)
     # Make case-insensitive (Canvas does not appear to support regex flags)
     r = re.sub(r"([A-Za-z])", lambda m: case_insensitive(m.group()), r)
     # Allow any amount / type of whitespace
