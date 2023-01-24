6e1c129d5b4bf700942d545ee1d181ca671d8210
Logan Walls <2934282+LoganWalls@users.noreply.github.com>
2023-01-24 09:38:15 -0500

    allow trailing punctuation and different Det forms

--- assignments/phrase-structure-grammars/answer_to_regex.py
+++ assignments/phrase-structure-grammars/answer_to_regex.py
@@ -7,11 +7,24 @@ def case_insensitive(char: str) -> str:
     return ""
 
 
-def convert(answer: str) -> str:
+def convert(answer: str, include_lex: bool) -> str:
+    r = answer
+    if not include_lex:
+        # Remove lexical items if not included
+        r = re.sub(r"\[\s*([^\s\[\]]+)[^\[\]]+\]", r"[\1]", r)
     # Add spacing around brackets
-    r = re.sub(r"([\[\]])", r" \1 ", answer)
+    r = re.sub(r"([\[\]])", r" \1 ", r)
     # Escape special characters
     r = re.sub(r"([^\sa-zA-Z/])", lambda m: f"{re.escape(m.group())}", r)
+    # Allow different versions of Det (D, Det, Det., etc.)
+    r = re.sub(
+        r"([^a-zA-Z])d(et)?([^a-zA-Z])",
+        r"\1d(et\.?)?\3",
+        r,
+        flags=re.IGNORECASE
+    )
+    # Allow a punctuation mark at the end of the sentence.
+    r = re.sub(r"(.+[A-Za-z])([^A-Za-z]*)$", r"\1[.!?]?\2", r)
     # Canvas uses ruby regex, so forward slashes need to be manually escaped.
     r = r.replace("/", "\\/")
     # Make case-insensitive (Canvas does not appear to support regex flags)
@@ -25,4 +38,5 @@ if __name__ == "__main__":
 
     with open(argv[1], "r") as f:
         answer = f.read().strip()
-    print(convert(answer))
+    include_lex = len(argv) <= 2 or argv[2].lower() == "true"
+    print(convert(answer, include_lex))
